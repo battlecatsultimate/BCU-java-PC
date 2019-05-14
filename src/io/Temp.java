581905b7892f2b0658e894371947d470fa65a3ac
lcy0x1 <arthurlcy0x1@gmail.com>
2019-05-13 21:13:36 -0700

    Update Temp.java

--- src/io/Temp.java
+++ src/io/Temp.java
@@ -5,15 +5,6 @@ import java.io.FileNotFoundException;
 
 public class Temp {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		File f = new File("./img/temp.txt");
-		Writer.check(f);
-		WriteStream ws = new WriteStream(f);
-		System.setErr(ws);
-		new Exception("hi").printStackTrace();
-		System.out.println(ws.writed);
-	}
-
 	public static void main$0(String[] args) {
 		WebFileIO.download("http://battlecatsultimate.cf/api/resources/assets/080504.zip", new File("./img/Test.zip"),
 				null);
