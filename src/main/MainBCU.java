b78083bf6e8d1c1c6ebaa95f42f0cdc7e473ad4e
MandarinSmell <geawonlee@naver.com>
2020-08-02 04:13:06 +0900

    fix saving bug

--- src/main/MainBCU.java
+++ src/main/MainBCU.java
@@ -29,7 +29,7 @@ public class MainBCU {
 
 	public static int FILTER_TYPE = 0;
 	public static final boolean WRITE = !new File("./.project").exists();
-	public static boolean preload = false, trueRun = false, loaded = false, USE_JOGL = false;
+	public static boolean preload = false, trueRun = true, loaded = false, USE_JOGL = false;
 	public static boolean light = false, nimbus = true;
 
 	public static String getTime() {
@@ -37,6 +37,13 @@ public class MainBCU {
 	}
 
 	public static void main(String[] args) {
+		trueRun = true;
+		long mem = Runtime.getRuntime().maxMemory();
+		if (mem >> 28 == 0) {
+			Opts.pop(Opts.MEMORY, "" + (mem >> 20));
+			System.exit(0);
+		}
+		
 		Writer.logPrepare();
 		Writer.logSetup();
 		Reader.getData$0();
