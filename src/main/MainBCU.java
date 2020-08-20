2c9e488e5cb5490d4b1d48117255b98e34714fd9
MandarinSmell <geawonlee@naver.com>
2020-08-20 21:53:18 +0900

    0-4-10-8

--- src/main/MainBCU.java
+++ src/main/MainBCU.java
@@ -25,7 +25,7 @@ import utilpc.awt.PCIB;
 
 public class MainBCU {
 
-	public static final int ver = 41007;
+	public static final int ver = 41008;
 
 	public static int FILTER_TYPE = 0;
 	public static final boolean WRITE = !new File("./.project").exists();
