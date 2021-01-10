f4c0b2455d15a8b8eba37ae30d82f2dcb918f1ae
MandarinSmell <mandarinsmellbcu@gmail.com>
2021-01-11 04:39:11 +0900

    0-5-0-4

--- src/main/MainBCU.java
+++ src/main/MainBCU.java
@@ -134,7 +134,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 50003;
+	public static final int ver = 50004;
 
 	public static int FILTER_TYPE = 1;
 	public static final boolean WRITE = !new File("./.project").exists();
