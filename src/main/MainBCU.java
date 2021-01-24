3f3a011c36ab1aa60e69b858c1d8eae652c49653
iREDMe <foodrickme@gmail.com>
2021-01-23 20:59:22 -0800

    0-5-0-14

--- src/main/MainBCU.java
+++ src/main/MainBCU.java
@@ -139,7 +139,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 50013;
+	public static final int ver = 50014;
 
 	public static int FILTER_TYPE = 1;
 	public static final boolean WRITE = !new File("./.project").exists();
