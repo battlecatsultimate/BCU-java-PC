35c396e640c88951affddaa4c9e4339ee53c5729
iREDMe <foodrickme@gmail.com>
2021-04-10 01:06:34 -0700

    clean code

--- src/main/java/page/Page.java
+++ src/main/java/page/Page.java
@@ -8,7 +8,6 @@ import utilpc.PP;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.function.Consumer;
