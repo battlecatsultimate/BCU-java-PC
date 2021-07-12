0f0e68d0c37b1e7ff660195d679d5d72ba1ef30c
iREDMe <foodrickme@gmail.com>
2021-07-12 09:33:56 -0700

    fix custom enemy drop rounding error

--- src/main/java/page/info/edit/EnemyEditPage.java
+++ src/main/java/page/info/edit/EnemyEditPage.java
@@ -48,7 +48,7 @@ public class EnemyEditPage extends EntityEditPage {
 			ce.limit = v[0];
 		}
 		if (jtf == fdr) {
-			ce.drop = (int) (v[0] / bas.t().getDropMulti());
+			ce.drop = (int) Math.round(v[0] / bas.t().getDropMulti());
 		}
 		if (jtf == fsr) {
 			if (v[0] < 0)
