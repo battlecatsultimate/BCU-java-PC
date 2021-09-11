a459b753a3041764a9dec95f77a67e3e14631cbf
iREDMe <foodrickme@gmail.com>
2021-09-11 14:46:54 -0700

    show unit limit in info page

--- src/main/java/page/info/UnitInfoTable.java
+++ src/main/java/page/info/UnitInfoTable.java
@@ -222,13 +222,15 @@ public class UnitInfoTable extends Page {
 		special[0][1].setText(f.du.getAtkLoop() < 0 ? "infinite" : f.du.getAtkLoop() + "");
 		special[0][2].setText(MainLocale.INFO, "width");
 		special[0][3].setText(f.du.getWidth() + "");
-		special[0][4].setText(MainLocale.INFO, "t7");
+		special[0][4].setText(MainLocale.INFO, "limit");
+		special[0][5].setText(f.du.getLim() + "");
+		special[0][6].setText(MainLocale.INFO, "t7");
 		int back = Math.min(f.du.getBack(), f.du.getFront());
 		int front = Math.max(f.du.getBack(), f.du.getFront());
 		if (back == front)
-			special[0][5].setText(back + "");
+			special[0][7].setText(back + "");
 		else
-			special[0][5].setText(Math.min(back, front) + " ~ " + Math.max(back, front));
+			special[0][7].setText(Math.min(back, front) + " ~ " + Math.max(back, front));
 		atks[0].setText("atk");
 		atks[2].setText(MainLocale.INFO, "preaa");
 		atks[4].setText(MainLocale.INFO, "use");
