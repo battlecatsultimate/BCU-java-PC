37fce13a8c7e9b6572d1385f552b5925fdcb6246
iREDMe <foodrickme@gmail.com>
2021-09-12 10:48:20 -0700

    correct default min. pos for created enemies

--- src/main/java/page/pack/PackEditPage.java
+++ src/main/java/page/pack/PackEditPage.java
@@ -323,7 +323,7 @@ public class PackEditPage extends Page {
 			changing = true;
 			CustomEnemy ce = new CustomEnemy();
 			AnimCE anim = jld.getSelectedValue();
-			ce.limit = 2.5 * anim.mamodel.confs[0][2] * 1.0 * (double) (anim.mamodel.parts[0][8] / anim.mamodel.ints[0]);
+			ce.limit = 2.5 * (-anim.mamodel.parts[0][6]) * 1.0 * (double) (anim.mamodel.parts[0][8] / anim.mamodel.ints[0]);
 			Enemy e = new Enemy(pac.getNextID(Enemy.class), anim, ce);
 			pac.enemies.add(e);
 			jle.setListData(pac.enemies.toRawArray());
