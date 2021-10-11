ed97572fbe05bef47105670e02a4cef59ff5f669
iREDMe <foodrickme@gmail.com>
2021-10-10 19:27:43 -0700

    fix checking v5 warning

--- src/main/java/io/BCUReader.java
+++ src/main/java/io/BCUReader.java
@@ -101,7 +101,7 @@ public class BCUReader extends DataIO {
 					CommonStatic.getConfig().backupFile = value.equals("None") ? null : value;
 				}
 				if(jo.has("ann0510")) {
-					MainBCU.announce0510 = jo.get("ann05010").getAsBoolean();
+					MainBCU.announce0510 = jo.get("ann0510").getAsBoolean();
 				}
 				String[] exp = JsonDecoder.decode(jo.get("export_paths"), String[].class);
 				String[] imp = JsonDecoder.decode(jo.get("import_paths"), String[].class);
