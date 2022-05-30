efdfa7103a2690b5f079a460cd955f8be1a6e75a
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2022-05-30 15:12:15 -0400

    0-5-1-14
    Yeah very fast release but there's stuff I have planned to do that will increase the core version for next update, and I wanted to get those 2 bug out of the way

--- src/main/java/main/MainBCU.java
+++ src/main/java/main/MainBCU.java
@@ -277,7 +277,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 50113;
+	public static final int ver = 50114;
 	private static final DecimalFormat df = new DecimalFormat("#.##");
 
 	public static byte FILTER_TYPE = 1;
