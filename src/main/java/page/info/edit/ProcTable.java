3c8a84d74376c44146b6d0e14eb971bc579a6b7c
Mewby64 <116971017+Mewby64@users.noreply.github.com>
2023-07-22 18:08:57 -0500

    More Mini Surge adding for customs

--- src/main/java/page/info/edit/ProcTable.java
+++ src/main/java/page/info/edit/ProcTable.java
@@ -21,10 +21,10 @@ public abstract class ProcTable extends Page {
 		private static final int SEC = 16;
 
 		private static final int[] UINDS = new int[] { Data.P_KB, Data.P_STOP, Data.P_SLOW, Data.P_CRIT, Data.P_WAVE, Data.P_MINIWAVE, Data.P_WEAK,  //Procs for units
-				Data.P_BREAK, Data.P_SHIELDBREAK, Data.P_WARP, Data.P_CURSE, Data.P_SATK, Data.P_POIATK, Data.P_VOLC, Data.P_BOUNTY, Data.P_ATKBASE,
+				Data.P_BREAK, Data.P_SHIELDBREAK, Data.P_WARP, Data.P_CURSE, Data.P_SATK, Data.P_POIATK, Data.P_VOLC, Data.P_MINIVOLC, Data.P_BOUNTY, Data.P_ATKBASE,
 				Data.P_SEAL, Data.P_SUMMON, Data.P_MOVEWAVE, Data.P_SNIPER, Data.P_BOSS, Data.P_TIME, Data.P_THEME, Data.P_POISON, Data.P_ARMOR, Data.P_SPEED };
 		private static final int[] EINDS = new int[] { Data.P_KB, Data.P_STOP, Data.P_SLOW, Data.P_CRIT, Data.P_WAVE, Data.P_MINIWAVE, Data.P_WEAK,  //Procs for enemies
-				Data.P_BREAK, Data.P_SHIELDBREAK, Data.P_WARP, Data.P_CURSE, Data.P_SATK, Data.P_POIATK, Data.P_VOLC, Data.P_ATKBASE, Data.P_SEAL,
+				Data.P_BREAK, Data.P_SHIELDBREAK, Data.P_WARP, Data.P_CURSE, Data.P_SATK, Data.P_POIATK, Data.P_VOLC, Data.P_MINIVOLC Data.P_ATKBASE, Data.P_SEAL,
 				Data.P_SUMMON, Data.P_MOVEWAVE, Data.P_SNIPER, Data.P_BOSS, Data.P_TIME, Data.P_THEME, Data.P_POISON, Data.P_ARMOR, Data.P_SPEED };
 
 		protected AtkProcTable(Page p, boolean edit, boolean unit) {
