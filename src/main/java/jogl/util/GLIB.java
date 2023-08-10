d42b964ca38548798e969b3dc4573f64f6db4924
RogueRouge <foodrickme@gmail.com>
2023-08-09 21:56:40 -0700

    add placeholder for dynamic texture offset

--- src/main/java/jogl/util/GLIB.java
+++ src/main/java/jogl/util/GLIB.java
@@ -21,6 +21,11 @@ public class GLIB extends ImageBuilder<BufferedImage> {
 	}
 
 	@Override
+	public FakeImage build(BufferedImage o, int offsetX, int offsetY) {
+		return null;
+	}
+
+	@Override
 	public FakeImage build(int w, int h) {
 		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB_PRE);
 
