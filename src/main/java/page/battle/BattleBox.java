feed0d6180ede2fd22996f7cd52efea907cc5bc5
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2024-01-03 13:15:18 +0900

    make "max" cost display gone

--- src/main/java/page/battle/BattleBox.java
+++ src/main/java/page/battle/BattleBox.java
@@ -454,8 +454,8 @@ public interface BattleBox {
 
 						g.colRect(x + iw - dw - xw, y + ih - dh * 2, xw, dh, 0, 0, 0, -1);
 						g.colRect(x + dw, y + ih - dh * 2, iw - dw * 2 - xw, dh, 100, 212, 255, -1);
-					} else if (!sb.summonerSummoned[i][j]) {
-						Res.getCost(pri == -1 ? -1 : pri / 100, !b, setSym(g, hr, x + iw, y + ih, 3));
+					} else if (pri != -1 && !sb.summonerSummoned[i][j]) {
+						Res.getCost(pri / 100, !b, setSym(g, hr, x + iw, y + ih, 3));
 					}
 				}
 			}
@@ -564,8 +564,8 @@ public interface BattleBox {
 
 						g.colRect(x + iw - dw - xw, y + ih - dh * 2, xw, dh, 0, 0, 0, -1);
 						g.colRect(x + dw, y + ih - dh * 2, iw - dw * 2 - xw, dh, 100, 212, 255, -1);
-					} else if (!sb.summonerSummoned[index][i]) {
-						Res.getCost(pri == -1 ? -1 : pri / 100, !b, setSym(g, hr, x + iw, y + ih, 3));
+					} else if (pri != -1 && !sb.summonerSummoned[index][i]) {
+						Res.getCost(pri / 100, !b, setSym(g, hr, x + iw, y + ih, 3));
 					}
 				}
 			}
