eccd63ca64be80c8ced1f3be01d0ec05ffa25a3d
Vivian the Zombie Queen <drakexw05@gmail.com>
2026-03-28 20:35:31 -0500

    replaced deprecated code

--- src/main/java/io/BCUReader.java
+++ src/main/java/io/BCUReader.java
@@ -59,10 +59,17 @@ public class BCUReader extends DataIO {
 		File f = new File(CommonStatic.ctx.getBCUFolder(), "./user/config.json");
 		if (f.exists()) {
 			try (Reader r = new InputStreamReader(Files.newInputStream(f.toPath()), StandardCharsets.UTF_8)) {
+
 				JsonElement je = JsonParser.parseReader(r);
+
 				r.close();
 				Config cfg = CommonStatic.getConfig();
+
+				// for some reason je isn't a json object
+				System.out.print(je.isJsonObject());
+
 				JsonDecoder.inject(je, Config.class, cfg);
+
 				JsonObject jo = je.getAsJsonObject();
 				int[] rect = JsonDecoder.decode(jo.get("crect"), int[].class);
 				MainFrame.crect = new Rectangle(rect[0], rect[1], rect[2], rect[3]);
