4df16684245cf566dfb52af9c1d136ce26630996
iRed <foodrickme@gmail.com>
2026-04-05 23:10:55 -0700

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,12 +64,12 @@ publishing {
     }
 }
 
-tasks.withType<JavaCompile>() {
+tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.release = 8
 }
 
-tasks.withType<Javadoc>() {
+tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
 
