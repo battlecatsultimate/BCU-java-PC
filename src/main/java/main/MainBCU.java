f8364ea3ff64cbfb0252d66b76a2dffc5ce6c2dc
iRed <foodrickme@gmail.com>
2026-05-28 13:54:03 -0700

    0-5-7-2

--- src/main/java/main/MainBCU.java
+++ src/main/java/main/MainBCU.java
@@ -300,7 +300,7 @@ public class MainBCU {
 		}
 	}
 
-	public static final int ver = 50701;
+	public static final int ver = 50702;
 	public static final boolean isPrerelease = false;
 	private static final DecimalFormat df = new DecimalFormat("#.##");
 
